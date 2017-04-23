f5067ee21c764e52e7827931bdcb58b6ed396629
LCK <LCK@INCONSPICUOUS>
2017-04-23 22:44:43 +0800

    Add project files.

---
+++ MySecondDiscordBot/CommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace MySecondDiscordBot
+{
+    public class CommandHandler
+    {
+        private CommandService commands;
+        private DiscordSocketClient client;
+        private IDependencyMap map;
+
+        public async Task Install(IDependencyMap _map)
+        {
+            //create Command Service, inject it to Dependency map
+            client = _map.Get<DiscordSocketClient>();
+            commands = new CommandService();
+
+            _map.Add(commands);
+
+            map = _map;
+
+            //Adds assembly to command service
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            client.MessageReceived += HandleCommand;
+        }
+
+        private async Task HandleCommand(SocketMessage parameterMessage)
+        {
+            //Don't handle the command if its a system message
+            var message = parameterMessage as SocketUserMessage;
+            if (message == null) return;
+
+            //Mark where prefix ends and command begins
+            int argPos = 0;
+
+            //Determine if message has a valid prefix, and adjust argPos
+            if (!(message.HasMentionPrefix(client.CurrentUser, ref argPos) || message.HasCharPrefix('!', ref argPos))) return;
+
+            //Create a Command Context
+            var context = new CommandContext(client, message);
+
+            //Execute the command, store the result
+            var result = await commands.ExecuteAsync(context, argPos, map);
+
+            //If the command failed, notify the user;
+            if (!result.IsSuccess)
+                await message.Channel.SendMessageAsync($"**Error:** {result.ErrorReason}");
+        }
+    }
+}
---
+++ MySecondDiscordBot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace MySecondDiscordBot
+{
+    public class Program
+    {
+        static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+
+        private async Task Start()
+        {
+            //Instatiate WebSocket client
+            client = new DiscordSocketClient();
+
+            var token = "Token Here Guys";
+
+            //Connect to discord
+            await client.LoginAsync(TokenType.Bot, token, true);
+            await client.StartAsync();
+
+            var map = new DependencyMap();
+            map.Add(client);
+
+            handler = new CommandHandler();
+            await handler.Install(map);
+
+            //Block this program until this is closed;
+            await Task.Delay(-1);
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine( msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
